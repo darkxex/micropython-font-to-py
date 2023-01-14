2c83a8fb33968fbe7a9c7dcb8b0c85731a0e7eeb
Angel Toloza <darkxex@gmail.com>
2023-01-14 00:48:56 -0300

    cambios

---
+++ batch.bat
@@ -0,0 +1,3 @@
+python font_to_py.py -x -k extended arial.ttf 20 arial20esp.py
+python font_to_py.py -x -k extended arial.ttf 10 arial10esp.py
+python font_to_py.py -x -k extended arial.ttf 30 arial30esp.py
